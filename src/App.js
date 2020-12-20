591e4950f69e3b42c700c24ce9fcc87776f257a7
Vardhu2706 <ayyagari.vardhu@gmail.com>
2020-12-20 10:45:18 +0530

    Installed React Router Dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
 
 // Functional Component
 function App() {
