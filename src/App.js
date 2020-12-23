726266ae9c1e761ae8aecdf8f43eeb9bd6c12215
Vardhu2706 <ayyagari.vardhu@gmail.com>
2020-12-23 14:02:42 +0530

    Added Shop Page & Content.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 // Imports
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component'
-import {Switch, Route} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-const HatsPage = () => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
 
 // Functional Component
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route  path='/hats' component={HatsPage}/>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
