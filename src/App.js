962e0eeb1a80ce9727cfd9e1dd684ed13ad6b43b
Vardhu2706 <ayyagari.vardhu@gmail.com>
2020-12-25 13:51:38 +0530

    Added Lifecycle Methods to get logged in user details.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import Homepage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-// Functional Component
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/login" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+// Class Component
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+  // Lifecycle Method - ComponentDidMount
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    })
+  }
+  // Lifecycle Method - ComponentWillUnmount
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  // Render
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/login" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 // Exports
 export default App;
