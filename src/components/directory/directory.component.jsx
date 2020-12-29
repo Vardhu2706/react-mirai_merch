64753c1dea97ea55686bbf272cacba959d94c09a
Vardhu2706 <ayyagari.vardhu@gmail.com>
2020-12-29 18:58:18 +0530

    Updated app with Reselect and replaced cart selectors with memoized selectors.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,63 +1,61 @@
 // Imports
-import React from 'react'
-import MenuItem from "../menu-item/menu-item.component"
-import './directory.styles.scss'
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
 
 // Class Component
 // Need to store state value of Menu-Items
-class Directory extends React.Component{
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            sections : [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-        }
-    }
-    // Render Function
-    render(){
-      return(
-        <div className='directory-menu'>
-          {
-            this.state.sections.map(({ id, ...otherSectionProps}) => (
-              <MenuItem key={id} {...otherSectionProps}/>
-            ))
-          }
-        </div>
-      )
-    }
+class Directory extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "images/hats.png",
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+        {
+          title: "jackets",
+          imageUrl: "images/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+        {
+          title: "sneakers",
+          imageUrl: "images/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+        {
+          title: "womens",
+          imageUrl: "images/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+        {
+          title: "mens",
+          imageUrl: "images/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+  // Render Function
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 // Exports
 export default Directory;
